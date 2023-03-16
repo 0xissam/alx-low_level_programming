6f27264feaf896b64abf3be5eb5726a7e9e1f7e0
0xissam <hah305581@gmail.com>
2023-03-16 06:12:04 -0700

    task-8 first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
 return (1);
 }
