cccace3b18f3023c08f790e90f219d22548f30f0
0xissam <hah305581@gmail.com>
2023-03-16 06:20:12 -0700

    task-8 first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 41);
+char *m = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, m, 41);
 return (1);
 }
