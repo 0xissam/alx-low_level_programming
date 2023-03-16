91e63900202cec29bd4eb5b898c3a98e84582870
0xissam <hah305581@gmail.com>
2023-03-16 06:03:39 -0700

    task-8 first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print line using printf fonction to the standard error
+ *  Return: 1
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+return (1);
+}
