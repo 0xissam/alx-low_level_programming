cef2e3ab82ebc3a401027acefea029e5c710b7cc
0xissam <hah305581@gmail.com>
2023-03-16 04:47:21 -0700

    task-5mfirst commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print line using printf fonction
+ * Return: 0 (sucses)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
