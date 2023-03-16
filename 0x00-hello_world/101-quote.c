fd0e24118da62f5930b26c7c98f383a36d9aaa7c
0xissam <hah305581@gmail.com>
2023-03-16 06:09:22 -0700

    task-8 first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - print line using printf fonction to the standard error
  *  Return: 1
  */
 int main(void)
 {
-printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
