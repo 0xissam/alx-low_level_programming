2081e3a99c5d0702962c977c78de175837a4dc3f
0xissam <hah305581@gmail.com>
2023-03-16 03:21:55 -0700

    task-4 first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 /*
- program that prints \"Programming is like building a multilingual puzzle
+ * program that prints \"Programming is like building a multilingual puzzle
 */
 
 #include <stdio.h>
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
