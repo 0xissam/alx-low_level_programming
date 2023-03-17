5a4ef73588432a27b3809f3a96b72ceed2f3940c
0xissam <hah305581@gmail.com>
2023-03-17 05:49:27 -0700

    task-6 first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers of base 10 using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Print digits 0 to 4 */
+	putchar(48);
+	putchar(49);
+	putchar(50);
+	putchar(51);
+	putchar(52);
+
+	/* Print digits 5 to 9 */
+	putchar(53);
+	putchar(54);
+	putchar(55);
+	putchar(56);
+	putchar(57);
+
+	/* Print a new line */
+	putchar('\n');
+
+	return (0);
+}
+
