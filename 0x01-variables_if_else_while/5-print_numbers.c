9fc6293e8c1238ec998668152cff5ae6fc56d031
0xissam <hah305581@gmail.com>
2023-03-17 05:46:54 -0700

    task-5 first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers of base 10 using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
