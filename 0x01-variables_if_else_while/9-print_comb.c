8ca40a3cf29d5e563e876d4d2f555144a61404ef
0xissam <hah305581@gmail.com>
2023-03-17 06:40:51 -0700

    task-9 first commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,20 +8,14 @@
 
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = i + 1; j < 10; j++)
-		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
 	}
+	putchar('9');
 	return (0);
 }
