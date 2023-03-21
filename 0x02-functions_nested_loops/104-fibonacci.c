2e7e71793c059975e5f3b84a6943ba6c598614fb
0xissam <hah305581@gmail.com>
2023-03-21 12:57:52 -0700

    task-16 first commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,19 +4,43 @@
  *
  * Return: Always 0 (Success)
  */
-
-int main(void)
+void print_time_table(int n)
 {
-	int i;
-	unsigned long int a = 1, b = 2, c;
+	int count;
+	unsigned long a = 0, b = 1, c;
+	unsigned long d, e, f, j;
+	unsigned long k, l;
 
-	printf("%lu, %lu", a, b);
-	for (i = 2; i < 98; i++)
+	for (count = 0; count < 92; count++)
 	{
 		c = a + b;
-		printf(", %lu", c);
+		printf("%lu, ",c);
 		a = b;
 		b = c;
+	}
+	d = a / 10000000000;
+	f = b / 10000000000;
+	e = a % 10000000000;
+	j = b % 10000000000;
+	for (count = 93; count < 99; count++)
+	{
+		k = d + f;
+		l = e = j;
+		if (d + j > 10000000000)
+		{
+			k += 1;
+			l %= 10000000000;
+
+		}
+		printf("%lu%lu",k , l);
+		if (count != 98)
+		{
+			printf(', ');
+			d = f;
+			e = j;
+			f = k;
+			j = l;
+		}
 	}
 	printf("\n");
 	return (0);
