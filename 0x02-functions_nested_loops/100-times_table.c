bf334d99a5affccb7e9668901862bb2deaee59c5
0xissam <89690781+0xissam@users.noreply.github.com>
2023-03-21 18:26:58 +0000

    task-12 first commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table starting from 0
- * @n: the highest number to multiply by
+ * print_times_table - Prints the n times table, starting with 0.
  *
- * Description: Prints the multiplication table up to n, starting from 0.
- * If n is greater than 15 or less than 0, the function does not print anything.
+ * @n: The number of times to print the table.
+ *
+ * Return: Nothing.
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int i, j, res;
 
 	if (n < 0 || n > 15)
 		return;
@@ -19,12 +19,25 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			k = i * j;
+			res = i * j;
+
 			if (j == 0)
-			printf("%d", k);
-		else
-			printf("\t%d", k);
+				printf("%d", res);
+			else
+			{
+				printf(", ");
+
+				if (res < 10)
+					printf("   ");
+				else if (res < 100)
+					printf("  ");
+				else
+					printf(" ");
+				
+				printf("%d", res);
+			}
 		}
+
 		printf("\n");
 	}
 }
