aa10656d1e7f665e92144d8753f19cb8c90ebd74
0xissam <hah305581@gmail.com>
2023-03-21 07:22:33 -0700

    task-12 first commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,6 +9,8 @@ void print_times_table(int n)
 {
 	int i, j;
 
+	if (n < 0 || n > 15)
+		return;
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -18,7 +20,7 @@ void print_times_table(int n)
 				printf("%d", i * j);
 				if (j != n)
 				{
-					printf(",\t");
+					printf(", ");
 				}
 			}
 			printf("\n");
