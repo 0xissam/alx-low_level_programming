16f38da62fcb406b52367393c0a58dcf9ec52da5
0xissam <hah305581@gmail.com>
2023-03-21 05:31:19 -0700

    task-2 first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(letter + j);
+		}
+		_putchar('\n');
+	}
+}
