8756e8607244d8c6752767f51398711b92fc2b6e
0xissam <hah305581@gmail.com>
2023-03-21 06:23:01 -0700

    task-9 first commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table.
+ */
+void times_table(void)
+{
+	int i, j, result;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j;
+			if (j == 0)
+			{
+				_putchar('0' + result);
+
+			}
+			else if (result < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
