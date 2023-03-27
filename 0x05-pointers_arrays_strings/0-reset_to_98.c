eb20cc8b999a74fe20509395e9a8e9e82136d70b
0xissam <hah305581@gmail.com>
2023-03-27 10:55:29 -0700

    task-0 first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * @n:  Pointer
+ *
+ * Return: void
+ */
+
+
+void reset_to_98(int *n)
+{
+
+		int *p = n;
+
+			*p = 98;
+
+
+
+}
