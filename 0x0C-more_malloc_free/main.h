55a8a1137df59150e9926f4684e07995af8cb347
0xissam <hah305581@gmail.com>
2023-04-13 17:27:44 -0700

    task-4 first commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
